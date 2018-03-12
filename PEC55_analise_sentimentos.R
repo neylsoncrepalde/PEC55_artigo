bf651c08707b6364bf740103fba82a9e7f32e3c6
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-03-12 11:29:47 -0300

    sentiment analysis of MBL part

--- PEC55_analise_sentimentos.R
+++ PEC55_analise_sentimentos.R
@@ -82,8 +82,49 @@ ggplot(na.omit(une_class %>% select(post_id, comment_sentiment_lex)),
 
 
 
-
 # MBL
-mbl_text = MBL$comment_message %>% tolower %>% removePunctuation %>%
-  removeWords(., stopwords("pt"))
-mbl_text = mbl_text %>% removeWords(., c("pra"))
+mbl_text = MBL %>% select(post_id, comment_message)
+mbl_text$comment_message = mbl_text$comment_message %>% tolower %>% removePunctuation %>%
+  removeWords(., stopwords("pt")) %>% removeNumbers
+mbl_text$comment_message = mbl_text$comment_message %>% removeWords(., "pra")
+
+# Dá id único aos comentários
+mbl_text %<>% mutate(comment_id = row_number())
+head(mbl_text)
+
+# Coloca cada termo em uma linha
+mbl_text_unnested = mbl_text %>% tidytext::unnest_tokens("term", "comment_message")
+head(mbl_text_unnested)
+
+# Fazendo merge com as polaridades
+mbl_text_unnested = mbl_text_unnested %>%
+  left_join(op30, by = "term") %>% 
+  left_join(sent %>% select(term, lex_polarity = polarity), by = "term") %>% 
+  select(post_id, comment_id, term, polarity, lex_polarity)
+
+mbl_text_unnested[1:100,]
+
+# Cria bd com os dois léxicos
+mbl_class = mbl_text_unnested %>% 
+  group_by(post_id) %>% 
+  summarise(
+    comment_sentiment_op = sum(polarity, na.rm = T),
+    comment_sentiment_lex = sum(lex_polarity, na.rm = T),
+    n_words = n()
+  )
+
+mbl_class[1:50,]
+# Plota OPLexicon
+ggplot(na.omit(mbl_class %>% select(post_id, comment_sentiment_op)), 
+       aes(x=post_id, y=comment_sentiment_op))+
+  geom_col()+scale_y_continuous(limits = c())+
+  scale_x_discrete(labels = NULL)+
+  labs(x="Post", y="Inclinação dos comentários", title="Reações dos comentários aos posts da MBL - OPLexicon 3.0")
+
+# Plota Sentilex
+ggplot(na.omit(mbl_class %>% select(post_id, comment_sentiment_lex)), 
+       aes(x=post_id, y=comment_sentiment_lex))+
+  geom_col()+scale_y_continuous(limits = c())+
+  scale_x_discrete(labels = NULL)+
+  labs(x="Post", y="Reação dos comentários", title="Reações dos comentários aos posts da MBL - SentiLex")
+
