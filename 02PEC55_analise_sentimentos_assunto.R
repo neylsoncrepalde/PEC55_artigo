df3f169d3519c79b5b23f0c17587f1a08d44fbf7
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-03-14 18:52:59 -0300

    start sentiment analysis by subject

---
+++ 02PEC55_analise_sentimentos_assunto.R
@@ -0,0 +1,10 @@
+### PEC 55 - Novas Análises
+### Maria Alice e Neylson
+### Script: Neylson
+### Análise de sentimentos
+###########################
+
+source("01PEC55_classifica_posts.R")
+
+# mbl_text_completo e une_text_completo
+
